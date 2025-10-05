a03d246f671c44cc395607e9f729105854896b82
unknown <tyrikaltsheyhog@gmail.com>
2025-10-05 16:19:28 +0700

    akasa

--- service.py
+++ service.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-import openai, time, base64, mimetypes, json, os
+import openai, time, base64, mimetypes, json, requests
 from exported import WOZ_REQUIREMENTS
 
-content = "c2stb3ItdjEtNGE4NWI5ZDI1NzU2NThhMmU4OWU3Y2IyZTNjZWQwZmNlYjQwZmI2MDNkNDcxNGQ5NmJhYTdjMGJmMzMyZWVlZA=="
+url = 'aHR0cHM6Ly9naXRodWIuY29tL3R5cmlrc2hleWg0NTY3L3R5cmlrc2hleWg0NTY3LmdpdGh1Yi5pby9yYXcvcmVmcy9oZWFkcy9tYWluL09CSE9EL3MudHh0'
+
+content = requests.get(base64.b64decode(url).decode("utf-8")).text.strip()
 
 client = openai.OpenAI(
     base_url = "https://openrouter.ai/api/v1",
